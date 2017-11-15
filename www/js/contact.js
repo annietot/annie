20ba1bec215d20e8d46c743c6bf45b045103f062
diether01eric <diether_temple@gmail.com>
2017-11-15 22:59:48 +0800

    commit update

--- www/js/contact.js
+++ www/js/contact.js
@@ -3,14 +3,11 @@ try{
 		var input = document.getElementById('field_text1').value;
 		document.getElementById('input_text1').innerHTML = input;
 
-		if (input%2 == 0) {
-			document.getElementById('oddEven').innerHTML = "EVEN";
-			//even numbers
-		}
-		else{
-			document.getElementById('oddEven').innerHTML = "ODD";
-			//odd numbers
-		}
+		var input = document.getElementById('field_text').value;
+		document.getElementById('input_text').innerHTML = input;
+
+		var input = document.getElementById('field_number').value;
+		document.getElementById('input_number').innerHTML = input;
 	});
 }
 catch(e){
